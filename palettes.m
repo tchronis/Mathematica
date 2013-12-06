f8b70c593c84a542da100c2ff0902c6d2249dea9
Theodore N. Chronis <t.n.chronis@gmail.com>
2013-12-06 15:14:23 +0200

    Update palettes.m

--- palettes.m
+++ palettes.m
@@ -9,3 +9,31 @@ With[{title = "Notebook selector"},
      Button["WindowTitle" /. NotebookInformation@#, 
         SetSelectedNotebook@#] & /@ Notebooks[], Button[title, _]]], 
   WindowTitle -> title]]
+  
+(*--------------------------------------------------------------------*)
+
+(*Open two copies of the same notebook*)
+(*http://mathematica.stackexchange.com/questions/9280/open-two-copies-of-the-same-notebook*)
+(*
+The code below creates a palette (which can be installed using the menu: Palettes/Install Palette) 
+containing a single button to create a duplicate of the active notebook. 
+The update button for the duplicate is located in the duplicate itself, leaving the original notebook unchanged. 
+The duplicate is closed using the normal window close button, but it has "ClosingSaveDialog" set to False so you 
+won't get a save prompt.
+*)
+CreatePalette[Button["Duplicate Active Notebook",
+   NotebookPut[NotebookGet[InputNotebook[]] /. 
+   {Rule[DockedCells, _] :> Sequence[], 
+    Rule[WindowMargins, _] :> Rule[WindowMargins, {{0, Automatic}, {0, Automatic}}], 
+    Cell[x___] :> Cell[x, Evaluatable -> False]}, 
+    Background -> GrayLevel[0.95], Editable -> False, "ClosingSaveDialog" -> False, 
+    DockedCells -> With[{sourcenb = InputNotebook[]}, 
+      Cell[BoxData[ToBoxes[Button["Update", 
+          SelectionMove[InputNotebook[], All, Notebook]; 
+          NotebookWrite[InputNotebook[], 
+           NotebookGet[sourcenb] /. Cell[x___] :> Cell[x, Evaluatable -> False]]]]],
+       "DockedCell", CellContext -> Cell]],
+    WindowTitle -> "Duplicate of " <> AbsoluteOptions[InputNotebook[], WindowTitle][[1, 2]]];
+   SetSelectedNotebook[InputNotebook[]]], WindowTitle -> "Duplicate"];
+
+(*--------------------------------------------------------------------*)
