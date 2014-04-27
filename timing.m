69d728bbaf7fcaefc2f36eb9e963207535e8fbca
Theodore N. Chronis <t.n.chronis@gmail.com>
2014-04-27 11:53:45 +0300

    Update timing.m

--- timing.m
+++ timing.m
@@ -1,6 +1,21 @@
-(*http://mathematica.stackexchange.com/questions/22419/benchmarking-expressions*)
-(*http://stackoverflow.com/questions/4198961/what-is-in-your-mathematica-tool-bag/4199042#4199042*)
+(*http://mathematica.stackexchange.com/a/32030/7966 *)
+(*This will print the total evaluation time for each cell as it completes in the lower left window margin:*)
 
-SetAttributes[timeAvg, HoldFirst]
+SetOptions[$FrontEndSession, EvaluationCompletionAction -> "ShowTiming"]
 
+
+(*http://mathematica.stackexchange.com/a/22427/7966 *)
+(*This performs enough repetitions of a given operation to exceed the threshold (0.3 seconds) with the aim getting a sufficiently stable and precise timing.*)
+
+SetAttributes[timeAvg, HoldFirst];
 timeAvg[func_] := Do[If[# > 0.3, Return[#/5^i]] & @@ Timing@Do[func, {5^i}], {i, 0, 15}]
+
+
+(*http://mathematica.stackexchange.com/a/7769/7966 *)
+(*You can put your Mathematica session in debug mode by going to Evaluation->Debugger
+Then, make some definitions and wrap the profiled code in RuntimeTools`Profile *)
+
+(*Example:
+f[x_] := x^2;
+Table[f[x], {100000}]; // RuntimeTools`Profile
+*)
